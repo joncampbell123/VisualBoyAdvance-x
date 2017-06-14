cae0b1c07e12ead70f33f5c20db2c68f011a5bd6
Jonathan Campbell <jonathan@castus.tv>
2017-06-13 22:45:15 -0700

    more

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./configure --prefix=/usr --enable-c-core --without-mmx || exit 1
+./configure --prefix=/usr --enable-c-core --without-mmx --disable-profiling || exit 1
 make -j5 || exit 1
 
